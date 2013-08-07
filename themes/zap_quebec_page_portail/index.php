7bde54e9a530cbc0cdc304204d6f18e132b8f60b
Marc Boivin <marc.boivin@libeo.com>
2013-08-06 20:22:35 -0400

    Ajouter les sidebars dans la page

--- themes/zap_quebec_page_portail/index.php
+++ themes/zap_quebec_page_portail/index.php
@@ -4,13 +4,14 @@
 	<h2>Bienvenue sur le réseau de ZAP Québec</h2>
 	<div id="blogcontent">
 		<?php do_action('zap_before_content'); ?>
-		<?php dynamic_sidebar('portail_content'); ?>
+		<?php dynamic_sidebar('home_right_1'); ?>
 		<?php do_action('zap_after_content'); ?>
 	</div>
 	
 </section>
 
 <section id="nouvelles-zap">
+        <?php dynamic_sidebar('home_left_1'); ?>
 	<?php 
 	
 			$rss = new WP_Widget_RSS();
